f95fba3ac6ddb16d0d74aca6b3fc8ca9c67b3cf8
justin <justinac.1999@gmail.com>
2023-11-24 17:18:46 +1000

    Headers and updated gitignore

---
+++ include/commotion/math.hpp
@@ -0,0 +1,89 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+
+#include <iostream>
+#include <cassert>
+
+#include "precision.hpp"
+
+
+namespace Commotion {
+    
+class Vector2 {
+public:
+    real x;
+    real y;
+
+    Vector2();
+    Vector2(real x, real y);
+    
+    void operator=(const Vector2& other);
+    Vector2 operator-(const Vector2& other);
+    Vector2 operator+(const Vector2& other);
+    Vector2 operator*(const Vector2& other);
+    Vector2 operator-(real scalar);
+    Vector2 operator+(real scalar);
+    Vector2 operator*(real scalar);
+
+    void operator-=(const Vector2& other);
+    void operator+=(const Vector2& other);
+    void operator*=(const Vector2& other); 
+    void operator-=(real scalar);
+    void operator+=(real scalar);
+    void operator*=(real scalar);
+
+    void invert();
+    real square_magnitude();
+    real magnitude();
+    real dot(const Vector2& other);
+    Vector2 normalize();
+
+    void print();
+private:
+    Vector2 operator/(real scalar);
+    void operator/=(real scalar);
+};
+
+    
+class Vector3 {
+public:
+    real x;
+    real y;
+    real z;
+
+    Vector3();
+    Vector3(real x, real y, real z);
+
+    void operator=(const Vector3& other);
+    Vector3 operator-(const Vector3& other);
+    Vector3 operator+(const Vector3& other);
+    Vector3 operator*(const Vector3& other);
+    Vector3 operator-(real scalar);
+    Vector3 operator+(real scalar);
+    Vector3 operator*(real scalar);
+    
+    void operator-=(const Vector3& other);
+    void operator+=(const Vector3& other);
+    void operator*=(const Vector3& other); 
+    void operator-=(real scalar);
+    void operator+=(real scalar);
+    void operator*=(real scalar);
+
+    void invert();
+    real square_magnitude();
+    real magnitude();
+    real dot(const Vector3& other);
+    Vector3 normalize();
+    Vector3 cross(const Vector3& other);
+    
+    void print();
+private:
+    Vector3 operator/(real scalar);
+    void operator/=(real scalar);
+};
+
+}
+
+
+#endif // MATH_HPP
---
+++ include/commotion/point_mass.hpp
@@ -0,0 +1,32 @@
+#ifndef POINT_MASS_HPP
+#define POINT_MASS_HPP
+
+
+#include <math.h>
+#include <commotion/math.hpp>
+#include <commotion/precision.hpp>
+
+
+namespace Commotion {
+
+class PointMass {
+public:
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
+    Vector2 forceAccum;
+    
+    real damping;
+    real mass; // kg
+
+    PointMass(const Vector2& position, real damping, real mass);
+
+    void add_force(const Vector2& force);
+    void clear_force_accum();
+    
+    void integrate(real delta_time);
+};
+
+}
+
+#endif // POINT_MASS_HPP
